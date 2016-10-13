525bf9f92878c9e0f70e9b1066853290d49e548a
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-10-13 15:32:52 +0000

    Version 0.1

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 setup(
     name = 'flasksr',
     packages=find_packages(exclude=('tests', 'tests.*')),
-    version = '0.0.1', 
-    description = 'Flask streaming response',
+    version = '0.1',
+    description = 'An easy way to stream HTTP Responses for Flask and improve time for First Paint',
     author = 'Arpit Bhayani',
     author_email = 'arpit.bhayani@gmail.com',
     url = 'https://github.com/arpitbbhayani/flasksr',
