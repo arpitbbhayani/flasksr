6f9fdba70eb2673caa066fd98b2d608193de3fc1
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2016-10-20 05:52:49 +0000

    Bump up version to 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'flasksr',
     packages=find_packages(exclude=('tests', 'tests.*')),
-    version = '0.1',
+    version = '0.2',
     description = 'An easy way to stream HTTP Responses for Flask and improve time for First Paint',
     author = 'Arpit Bhayani',
     author_email = 'arpit.bhayani@gmail.com',
